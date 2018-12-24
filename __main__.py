9a06c434811d3659a204b4fdb8ce873681340569
Timofey Kuzmin <sinnerpocht@yandex.ru>
2018-12-24 22:00:18 +0300

    Add crop for old spritesheets format

--- __main__.py
+++ __main__.py
@@ -3,8 +3,14 @@
 from classmodule import ConvertAtlas
 
 # Объект с координатами, шириной и высотой
-json_path = 'main_atlas.json'
-img_path = 'main_atlas.png'
+# Для формата атласа указывается
+# json_path = 'main_atlas.json'
+# img_path = 'main_atlas.png'
+
+#  Для формата старого spritesheet нужно указать
+#  frameWidth: 65.6,
+#  frameHeight: 65.6,
+# Количество фреймов  frames count 4
 
 if __name__ == '__main__':
-    ConvertAtlas(json_path, img_path)
+    ConvertAtlas()
--- classmodule.py
+++ classmodule.py
@@ -7,39 +7,77 @@ import argparse
 class ConvertAtlas:
     """
     :todo: Не перезаписывать существующее изображение, выдавать предупреждение?
-    :todo:
+    :todo: Получать в качестве аргумента название папки для сохранения вырезаемых картинок
+    :todo: Обработка возможных ошибок - не тот формат файлов:
     """
 
-    def __init__(self, json_path, img_path):
+    def __init__(self):
         parser = argparse.ArgumentParser(description="Convert texture packer atlas of images to separate images")
-        parser.add_argument('json',
-                            help='path to the json configuration file')
         parser.add_argument('img',
                             help='path to the atlas image')
-        args = parser.parse_args()
+        parser.add_argument('--json',
+                            help='path to the json configuration file')
+        # Флаг для переключения типа изображения и конфигурационного файла - поддерживает как современный формат
+        # атласа, используемого в Texture Packer, так и стандартные spritesheets
+        parser.add_argument('--format', '-f', default="atlas",
+                            help='Which type of image used - json and atlas or old type spritesheets')
 
-        if not os.path.exists(args.json):
-            print('Ошибка! Файл {} не существует!'.format(args.json))
-            return
-        elif not os.path.exists(args.img):
+        parser.add_argument('--width', '-w', help='Width of one frame (only for spritesheets)', type=float)
+
+        parser.add_argument('--height', help='Height of one frame (only for spritesheets)', type=float)
+
+        parser.add_argument('--count', '-c', help='Count of frames (only for spritesheets)', type=int)
+
+        args = parser.parse_args()
+        image_format = args.format
+        print(image_format)
+
+        if not os.path.exists(args.img):
             print('Ошибка! Файл изображения {} не существует!'.format(args.img))
             return
 
-        self.json_path = args.json
         self.img_path = args.img
 
-        with open(self.json_path) as data_file:
-            data = json.load(data_file)
-            self.img = Image.open(self.img_path)
-            for frame_name in data['frames']:
-                dir_name = os.path.dirname(frame_name)  # Получаем имя директории
-                file_name = os.path.splitext(frame_name)[0]  # Получаем имя файла без расширения
-                file_name = os.path.split(file_name)[-1]  # Отделение имени файла от названия директории
-                img_data = data["frames"].get(frame_name)
-                self.crop(img_data, dir_name, file_name)
-                # cropped_img.show() # Показать изображение
+        if image_format == 'atlas':
+            if not os.path.exists(args.json):
+                print('Ошибка! Файл {} не существует!'.format(args.json))
+                return
+
+            self.json_path = args.json
+
+            with open(self.json_path) as data_file:
+                data = json.load(data_file)
+                self.img = Image.open(self.img_path)
+                if not os.path.exists('images'):
+                    os.mkdir('images', mode=0o775)
+
+                for frame_name in data['frames']:
+                    dir_name = os.path.dirname(frame_name)  # Получаем имя директории
+                    file_name = os.path.splitext(frame_name)[0]  # Получаем имя файла без расширения
+                    file_name = os.path.split(file_name)[-1]  # Отделение имени файла от названия директории
+                    img_data = data["frames"].get(frame_name)
+                    self.crop(img_data, dir_name, file_name)
+
+        elif image_format == 'sprite' or type == 'spritesheet':
+            width = args.width
+            height = args.height
+            count = args.count
+
+            if not width or not height or not count:
+                print('Error! Spritesheets format required more atrguments: width, height and count')
+            else:
+                self.crop_spritesheet(width, height, count)
+        else:
+            print('Error! Unknown format of the image')
 
     def crop(self, img_data, dir_name, file_name):
+        """
+        Method for crop atlas of images by json data
+        :param img_data:
+        :param dir_name:
+        :param file_name:
+        :return:
+        """
 
         frame = img_data.get('frame')
         # Определяем точки прямоугольника, который нужно вырезать
@@ -51,9 +89,7 @@ class ConvertAtlas:
         bottom = top + height
         area = (left, top, right, bottom)
         cropped_img = self.img.crop(area)
-
-        if not os.path.exists('images'):
-            os.mkdir('images', mode=0o775)
+        # cropped_img.show() # Показать изображение
 
         if dir_name:
             dir_name = './images/{}'.format(dir_name)
@@ -65,3 +101,21 @@ class ConvertAtlas:
         # print(img_path)
         cropped_img.save(img_path)  # Сохраняем всё в png-формате
         print('Image {} successfully saved'.format(img_path))
+
+    def crop_spritesheet(self, width, height, count):
+
+        self.img = Image.open(self.img_path)
+
+        if not os.path.exists('images'):
+            os.mkdir('images', mode=0o775)
+
+        for i in range(count):
+            left = width*i
+            top = 0
+            right = left + width
+            bottom = top + height
+            area = (left, top, right, bottom)
+            cropped_img = self.img.crop(area)
+            img_path = 'images/img-{}.png'.format(i)
+            cropped_img.save(img_path)
+            print('Image {} successfully saved'.format(img_path))
